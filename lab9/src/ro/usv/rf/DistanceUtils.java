50b8779263b8e755d36b611fcc27e465e645e438
Calin Acroitoritei <acroitoritei@gmail.com>
2020-04-29 11:23:32 +0300

    Finished k-means algorithm

--- lab9/src/ro/usv/rf/DistanceUtils.java
+++ lab9/src/ro/usv/rf/DistanceUtils.java
@@ -1,17 +1,23 @@
 package ro.usv.rf;
 
+import java.awt.geom.Point2D;
+
 /**
  * DistanceUtils
  */
 public class DistanceUtils {
 
-    protected static double generalizedEuclidianDistance(double[] pattern1, double[] pattern2, int classOffset) {
+    protected static double generalizedEuclidianDistance(double[] pattern1, double[] pattern2) {
         double distance = 0.0;
         double diffSums = 0.0;
-        for (int i = 0; i < pattern1.length - classOffset; i++) {
+        for (int i = 0; i < pattern1.length; i++) {
             diffSums += Math.pow((pattern1[i] - pattern2[i]), 2);
         }
         distance = Math.sqrt(diffSums);
         return distance;
+    }
+
+    protected static double generalizedEuclidianDistance(Point2D p1, Point2D p2) {
+        return Math.sqrt((Math.pow(p1.getX() - p2.getX(), 2)) + (Math.pow(p1.getY() - p2.getY(), 2)));
     }
 }
--- lab9/src/ro/usv/rf/MainClass.java
+++ lab9/src/ro/usv/rf/MainClass.java
@@ -1,12 +1,13 @@
 package ro.usv.rf;
 
-import java.util.Arrays;
+import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class MainClass {
     public static void main(String[] args) {
         double[][] learningSet;
-
         try {
             learningSet = FileUtils.readLearningSetFromFile("in.txt");
             int numberOfPatterns = learningSet.length;
@@ -14,7 +15,10 @@ public class MainClass {
 
             System.out.println(String.format("The learning set has %s patters and %s features", numberOfPatterns,
                     numberOfFeatures));
-            dynamicKernels(learningSet, 2);
+            int[] classes = kMeans(learningSet, 2, 100, 0.0001);
+            for (int i = 0; i < classes.length; i++) {
+                System.out.println(classes[i]);
+            }
         } catch (USVInputFileCustomException e) {
             System.out.println(e.getMessage());
         } finally {
@@ -22,27 +26,87 @@ public class MainClass {
         }
     }
 
-    public static int[] dynamicKernels(double[][] learningSet, int k) {
-        Random rng = new Random();
-        int[] iClass = new int[learningSet.length];
-        int[] centroids = new int[k];
-        boolean done;
-        Arrays.fill(iClass, 0);
-
-        for (int i = 0; i < k; i++) {
-            centroids[i] = rng.nextInt(k);
-            iClass[centroids[i]] = i + 1;
-            // System.out.println(centroids[i] + " " + iClass[centroids[i]]);
+    public static int[] kMeans(double[][] learningSet, int k, int max_iterations, double precision) {
+        List<Point2D> dataSet = new ArrayList<Point2D>();
+        ArrayList<Point2D> centroids = new ArrayList<Point2D>();
+        ArrayList<Point2D> oldCentroids = null;
+        List<ArrayList<Double>> distances = new ArrayList<ArrayList<Double>>();
+        int[] clusterIndex = new int[learningSet.length];
+        int iterations = 0;
+        Random r = new Random();
+        // Create a list of points
+        for (int i = 0; i < learningSet.length; i++) {
+            dataSet.add(new Point2D.Double(learningSet[i][0], learningSet[i][1]));
         }
 
-        do {
-            done = true;
-            for (int i = 0; i < k; i++) {
-                for (int j = 0; j < learningSet.length; j++) {
+        // Pick k random starting centroids
+        for (int i = 0; i < k; i++) {
+            int cIndex = r.nextInt(learningSet.length);
+            // Assign cluster id to this point
+            clusterIndex[cIndex] = i;
+            System.out.println(dataSet.get(cIndex));
+            centroids.add(dataSet.get(cIndex));
+        }
+        oldCentroids = new ArrayList<Point2D>(centroids);
 
+        // Start loop
+        do {
+
+            oldCentroids = new ArrayList<Point2D>(centroids);
+            iterations++;
+
+            // Calculate distances from each point to the centroids
+            for (int i = 0; i < k; i++) {
+                ArrayList<Double> dist = new ArrayList<Double>();
+                for (int j = 0; j < dataSet.size(); j++) {
+                    // System.out.println(DistanceUtils.generalizedEuclidianDistance(centroids.get(i),
+                    // dataSet.get(j)));
+                    dist.add(DistanceUtils.generalizedEuclidianDistance(centroids.get(i), dataSet.get(j)));
                 }
+                distances.add(dist);
             }
-        } while (done == false);
-        return iClass;
+
+            // Assign each point to a cluster
+            for (int point = 0; point < dataSet.size(); point++) {
+                double minDist = Double.POSITIVE_INFINITY;
+                int cIndex = -1;
+                for (int cl = 0; cl < k; cl++) {
+                    if (distances.get(cl).get(point) < minDist) {
+                        minDist = distances.get(cl).get(point);
+                        cIndex = cl;
+                    }
+                }
+                clusterIndex[point] = cIndex;
+            }
+
+            // Calculate new centroid values
+            for (int cl = 0; cl < k; cl++) {
+                int nrOfPoints = 0;
+                double[] coordSums = { 0, 0 };
+                for (int point = 0; point < dataSet.size(); point++) {
+                    if (clusterIndex[point] == cl) {
+                        nrOfPoints++;
+                        coordSums[0] += dataSet.get(point).getX();
+                        coordSums[1] += dataSet.get(point).getY();
+                    }
+                }
+
+                centroids.set(cl,
+                        new Point2D.Double((double) (coordSums[0] / nrOfPoints), (double) (coordSums[1] / nrOfPoints)));
+            }
+        } while (!shouldStop(oldCentroids, centroids, iterations, max_iterations, precision));
+        return clusterIndex;
     }
+
+    private static boolean shouldStop(ArrayList<Point2D> oldCentroids, ArrayList<Point2D> centroids, int iterations,
+            int max_iterations, double precision) {
+        if (iterations > max_iterations)
+            return true;
+        for (int i = 0; i < centroids.size(); i++) {
+            if (DistanceUtils.generalizedEuclidianDistance(oldCentroids.get(i), centroids.get(i)) < precision)
+                return true;
+        }
+        return false;
+    }
+
 }
